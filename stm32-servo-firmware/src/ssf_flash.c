c38795877d8d56c95ede13abc14e23335775efcf
Dömötör Gulyás <dognotdog@gmail.com>
2020-10-08 22:51:42 -0400

    improved flash debug output

--- stm32-servo-firmware/src/ssf_flash.c
+++ stm32-servo-firmware/src/ssf_flash.c
@@ -7,9 +7,14 @@
 
 void ssf_flashInit(void)
 {
+	size_t freeBytes = (uintptr_t)(__freeflash_end_addr - __freeflash_firstPage_addr);
+
 	dbg_println("Free Flash memory starts at 0x%08X", (uintptr_t)__freeflash_firstPage_addr);
 	dbg_println("Free Flash memory ends at   0x%08X", (uintptr_t)__freeflash_end_addr);
-	dbg_println("Free Flash memory contains  0x%08X (%u) bytes", (uintptr_t)(__freeflash_end_addr - __freeflash_firstPage_addr), (uintptr_t)(__freeflash_end_addr - __freeflash_firstPage_addr));
+	dbg_println("Free Flash memory contains  0x%08X (%u) bytes", freeBytes, freeBytes);
+
+	size_t numFreePages = freeBytes/FLASH_PAGE_SIZE;
+	dbg_println("                            0x%08X (%u) pages", numFreePages, numFreePages);
 
 
 }
